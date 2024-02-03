81bede6611a13c090b6fd640320a039bf94eb25f
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-03 11:09:03 +0000

    Add files via upload

---
+++ xlr-xray-plugin/jira/CheckIssue.py
@@ -0,0 +1,44 @@
+#
+# Copyright 2019 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+import sys
+import com.xhaus.jyson.JysonCodec as json
+import time
+
+ISSUE_RETREIVED_STATUS = 200
+
+if jiraServer is None:
+    print "No server provided."
+    sys.exit(1)
+
+request = HttpRequest(jiraServer, username, password)
+
+statusTask = '/rest/api/2/issue/' + issueId
+
+while True:
+    response = request.get(statusTask, contentType = 'application/json')
+
+    # if response received from Jira
+    if response.getStatus() == ISSUE_RETREIVED_STATUS:
+        # retrieve issue status
+        data = json.loads(response.getResponse())['fields']['status']
+        issueStatus = data['name']
+        print "\nIssue %s status is %s." % (issueId, issueStatus)
+        if issueStatus in expectedStatusList:
+            print "\nThe status of issue %s is %s" % (issueId, issueStatus)
+            finalStatus = issueStatus
+            break
+        else:
+            print "\nIssue is not correct status"
+            time.sleep(pollInterval)
+    else:
+        print "Error from JIRA, HTTP Return: %s" % (response.getStatus())
+        response.errorDump()
+        sys.exit(1)
---
+++ xlr-xray-plugin/jira/JiraServerExt.py
@@ -0,0 +1,127 @@
+#
+# Copyright 2019 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+import json
+from jira import JiraServer
+from util import error
+
+
+class JiraServerExt(JiraServer):
+    def checkQuery(self, query):
+        if not query:
+            error('No JQL query provided.')
+
+        # Create POST body
+        content = {
+            'jql': query,
+            'startAt': 0,
+            'fields': ['summary', 'status']
+        }
+
+        # Do request
+        request = self._createRequest()
+        response = request.post('/rest/api/2/search', self._serialize(content), contentType='application/json')
+        if response.status != 200:
+            error(u"Failed to execute search '{0}' in JIRA.".format(query), response)
+        data = json.loads(response.response)
+        counter = 0
+        issues = {}
+        while response.status == 200 and 100*counter < data["total"]:
+            data = json.loads(response.response)
+            for item in data['issues']:
+                issue = item['key']
+                issues[issue] = (item['fields']['summary'], item['fields']['status']['name'])
+            counter += 1
+            content['startAt'] = 100*counter
+            response = request.post('/rest/api/2/search', self._serialize(content), contentType='application/json')
+            if response.status != 200:
+                error(u"Failed to execute search '{0}' in JIRA.".format(query), response)
+        return issues
+
+
+    def getVersionIdsForProject(self, projectId):
+        print "Executing jira.getVersionIdsForProject\n"
+        if not projectId:
+            error("No project id provided.")
+        request = self._createRequest()
+        response = request.get(self._versionsUrl(projectId), contentType="application/json")
+        if response.status != 200:
+            error(u"Unable to find versions for project id %s" % projectId, response)
+        versionIds = []
+        for item in json.loads(response.response):
+            versionIds.append(item['id'])
+        print str(versionIds) + "\n"
+        print "Exiting jira.getVersionIdsForProject\n"
+        return versionIds
+
+    def _versionsUrl(self, projectId):
+        return "/rest/api/2/project/%s/versions" % projectId
+
+    def queryForFields(self, query, fields):
+        if not query:
+            error('No JQL query provided.')
+
+        # Create POST body
+        content = {
+            'jql': query,
+            'startAt': 0,
+            'fields': fields,
+            'maxResults': 1000
+        }
+
+        # Do request
+        request = self._createRequest()
+        response = request.post('/rest/api/2/search', self._serialize(content), contentType='application/json')
+
+        # Parse result
+        if response.status == 200:
+            data = json.loads(response.response)
+
+            issues = []
+            for item in data['issues']:
+                issue = {}
+                for field in fields:
+                    issue[field] = item[field]
+                issues.append(issue)
+            return issues
+        else:
+            error(u"Failed to execute search '{0}' in JIRA.".format(query), response)
+
+    def queryForIssueIds(self, query):
+        issues = self.queryForFields(query, ["id"])
+        # for backwards compatibility, return a flat list with ids
+        return [x["id"] for x in issues]
+
+    def get_boards(self, board_name):
+        if not board_name:
+            error("No board name provided.")
+        request = self._createRequest()
+        response = request.get("/rest/agile/1.0/board?name=%s" % board_name, contentType="application/json")
+        if response.status != 200:
+            error(u"Unable to find boards for {0}".format(board_name), response)
+        return json.loads(response.response)['values']
+
+    def get_all_sprints(self, board):
+        if not board:
+            error("No board id provided")
+        request = self._createRequest()
+        response = request.get("/rest/agile/1.0/board/%s/sprint" % board["id"], contentType="application/json")
+        sprints = {}
+        if response.status != 200:
+            error(u"Unable to find sprints for board {0}".format(board["name"]), response)
+        sprints_json = json.loads(response.response)['values']
+        for sprint_json in sprints_json:
+            sprints[sprint_json["name"]] = sprint_json["id"]
+            print "| %s | %s | %s | %s |" % (sprint_json["name"], sprint_json["id"],
+                                             sprint_json["startDate"] if sprint_json.has_key(
+                                                 "startDate") else "not defined",
+                                             sprint_json["endDate"] if sprint_json.has_key(
+                                                 "endDate") else "not defined")
+        return sprints
